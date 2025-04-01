b472313e49e9df250fc0e57f4eecbba529de40a2
alumno <alumno@Instituto.local>
2025-04-01 09:55:47 -0300

    RepasoHTML-Css-JS v4.0
    hola

---
+++ funcion.js
@@ -0,0 +1,9 @@
+let cambiarColor = document.getElementById("cambiar-color")
+let boton = document.getElementById("boton")
+
+function cambiarColor(){
+    if (boton.onclick){
+        cambiarColor.style.color("green")
+    }
+    
+}
